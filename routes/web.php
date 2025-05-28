b2da1e2555d63a2dbf5f76fe492ff763a020fef8
brusnitsyn <brusnitsyn@gmail.com>
2025-05-29 08:12:15 +0900

    Временная зона + sse текущего времени

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,36 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('sse')->group(function () {
+    Route::get('time', function () {
+        return response()->stream(function () {
+            while (true) {
+
+                echo "data: " . json_encode([
+                        'time' => \Illuminate\Support\Carbon::now()->toDateTimeString(),
+                        'event' => 'ServerTimeEvent'
+                    ]) . "\n\n";
+
+                ob_flush();
+                flush();
+
+                sleep(1);
+
+                if (connection_aborted()) {
+                    break;
+                }
+            }
+        }, 200, [
+            'Content-Type' => 'text/event-stream',
+            'Cache-Control' => 'no-cache',
+            'Connection' => 'keep-alive',
+            'X-Accel-Buffering' => 'no'
+        ]);
+    });
 });
+
+Route::get('/{any?}', function () {
+    return view('app'); // Главный Blade-шаблон с Vue
+})->where('any', '.*');
+
+//Route::get('/config', [\App\Http\Controllers\ReverbAppController::class, 'config'])->name('config');
